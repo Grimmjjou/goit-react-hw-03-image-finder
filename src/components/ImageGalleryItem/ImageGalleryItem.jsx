53621e497e38b938b714275798c0e8a10e49507c
Grimmjjou <grimmjjou@gmail.com>
2023-05-12 21:31:58 +0300

    hom

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import styles from './ImageGalleryItem.module.css';
+
+export default function ImageGalleryItem({ url, tags, onClick }) {
+  return (
+    <>
+      <li className={styles.gallery_item}>
+        <img src={url} alt={tags} onClick={() => onClick(url)} />
+      </li>
+    </>
+  );
+}
+ImageGalleryItem.propTypes = {
+  url: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Modal.module.css';
+
+export default class Modal extends Component {
+  static propTypes = {
+    url: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
+  };
+  state = {};
+  componentDidMount() {
+    window.addEventListener('keydown', this.clickEsc);
+  }
+  componentWillUnmount() {
+    window.addEventListener('keydown', this.clickEsc);
+  }
+  clickBackdrop = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+  clickEsc = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return (
+      <div className={styles.overlay} onClick={this.clickBackdrop}>
+        <div className={styles.modal}>
+          <img src={this.props.url} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
